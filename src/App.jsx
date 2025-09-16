2779702f107b8d65d5f755159393378af0db2d1d
VedantBandre <vedantbandre33@gmail.com>
2025-09-16 18:12:26 +0200

    search and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,79 @@
-import React, { useEffect } from 'react'
-import Search from './components/search'
-import { useState } from 'react'
+import "./css/App.css";
+import React, { useEffect } from 'react';
+import Search from './components/search';
+import { useState } from 'react';
+import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
+import {Routes, Route} from "react-router-dom"
+import Favourites from './pages/Favourites';
+import NavBar from './components/NavBar';
 
-const API_BASE_URL = 'https://api.themoviedb.org/3';
-
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
-const API_OPTIONS = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`,
-  }
-}
-
-
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const fetchMovies = async () => {
-    try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-      
-      const response = await fetch(endpoint, API_OPTIONS);
-      
-      if(!response.ok){
-        throw new Error('Failed to fetch movies');
-      }
-
-    } catch (error) {
-      console.error(`Error fetching movies: ${error}`);
-      setErrorMessage('Error fetching movies. Please try again later.')
-    }
-  }
-  
-  useEffect(() => {
-
-  }, []);
-
+function App() {
   return (
-    <main>
-
-      <div className='pattern' />
-      <div className='wrapper'>
-        <header>
-          <img src='./banner.png' alt='banner'></img>
-          <h1>Find <span className='text-gradient'>Movies</span> to watch</h1>
-        
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-
-        </header>
-
-        <section className='all-movies'>
-          <h2>All Movies</h2>
-
-          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
-        </section>
-        
-      </div>
-    </main>
-  )
+    <div>
+      <NavBar></NavBar>
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favourites" element={<Favourites />}/>
+        </Routes>
+      </main>
+    </div>
+  );
 }
+
+
+// const App = () => {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   const fetchMovies = async () => {
+//     try {
+//       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      
+//       const response = await fetch(endpoint, API_OPTIONS);
+      
+//       if(!response.ok){
+//         throw new Error('Failed to fetch movies');
+//       }
+
+//       const data = await response.json();
+
+//       console.log(data);
+
+//     } catch (error) {
+//       console.error(`Error fetching movies: ${error}`);
+//       setErrorMessage('Error fetching movies. Please try again later.')
+//     }
+//   }
+  
+//   useEffect(() => {
+
+//   }, []);
+
+//   return (
+//     <main>
+
+//       <div className='pattern' />
+//       <div className='wrapper'>
+//         <header>
+//           <img src='./banner.png' alt='banner'></img>
+//           <h1>Find <span className='text-gradient'>Movies</span> to watch</h1>
+        
+//           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+//         </header>
+
+//         <section className='all-movies'>
+//           <h2>All Movies</h2>
+
+//           {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+//         </section>
+        
+//       </div>
+//     </main>
+//   )
+// }
 
 export default App
---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favourites.css";
+
+function Favourite() {
+    return <div className="favourites-empty">
+        <h2>No  Favourite Movies Yet</h2>
+        <p>Add movies to favourites in order for them to appear here</p>
+    </div>
+}
+
+export default Favourite
